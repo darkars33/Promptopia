c882af16083c6e2b76a6eaacef4503306a195e35
darkars33 <darshangarad18@gmail.com>
2024-09-12 13:22:28 +0530

    init all

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import '@/styles/globals.css'
+
+
+export const metadata = {
+          title: "Promptopia",
+          description: "The best place to find AI prompts.",
+}
+
+const RootLayout = () => {
+  return (
+    <html lang='en'>
+          <body>
+                    <div className='main'>
+                              
+                    </div>
+          </body>
+    </html>
+  )
+}
+
+export default RootLayout
