4c50cb2513798b2807ac1f7eeeaeaeb98d27abed
darkars33 <darshangarad18@gmail.com>
2024-09-29 22:14:39 +0530

    minor updates

--- components/Prompts.jsx
+++ components/Prompts.jsx
@@ -20,14 +20,15 @@ const Prompts = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     }, 3000);
   }
 
+  const name = post?.creator?.username[0];
 
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image src={post.creator.image} alt="alt" width={40} height={40}
+          {post?.creator?.image ? (<Image src={post.creator.image} alt="alt" width={40} height={40}
             className="rounded-full object-contain"
-          />
+          />): <p className="p-2 w-10 text-center h-10 bg-blue-300 rounded-full">{name}</p>}
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">{post?.creator?.username}</h3>
             <p className="font-inter text-sm text-gray-500">{post?.creator?.email}</p>
